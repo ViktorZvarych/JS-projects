8f6785938de05ac72a01abf8a4ef02bbdbc3f4b2
Viktor Zvarych <viktor.zvarych.qa@gmail.com>
2023-07-17 11:43:29 +0300

    changed counter and home link

--- projects/counter/counter.js
+++ projects/counter/counter.js
@@ -1,19 +1,27 @@
 const display = document.querySelector('.current-value');
 let currentValue = Number((document.querySelector('.current-value')).textContent);
 const btnMinus = document.querySelector('.btn-minus');
+const btnReset = document.querySelector('.btn-reset');
 const btnPlus = document.querySelector('.btn-plus');
 
 btnMinus.addEventListener('click', minusOne);
+btnReset.addEventListener('click', reset);
 btnPlus.addEventListener('click', plusOne);
 
 function minusOne () {
     console.log('-');
     currentValue --;
     display.textContent = currentValue;
-}
+};
 
 function plusOne () {
     console.log('+');
     currentValue ++;
     display.textContent = currentValue;
-}
+};
+
+function reset () {
+    console.log('0');
+    currentValue = 0;
+    display.textContent = currentValue;
+};
