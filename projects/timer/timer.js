c8c5d8e96b7f0095b35188c184de8e3761998768
Viktor Zvarych <viktor.zvarych.qa@gmail.com>
2023-07-18 14:36:09 +0300

    some changes in timer

--- projects/timer/timer.js
+++ projects/timer/timer.js
@@ -1,49 +1,55 @@
 (() => {
- 
-  const hours = document.getElementById('hours');
+  const hours = document.getElementById("hours");
   let currentHour = Number(hours.textContent);
   console.log(currentHour);
 
-  const minutes = document.getElementById('minutes');
+  const minutes = document.getElementById("minutes");
   let currentMinute = Number(minutes.textContent);
   console.log(currentMinute);
 
-  const seconds = document.getElementById('seconds');
+  const seconds = document.getElementById("seconds");
   let currentSecond = Number(seconds.textContent);
   console.log(currentSecond);
-
-function timer () {
-  if (currentSecond < 60) {
-    currentSecond++;
-  seconds.textContent = currentSecond;
-  }
-  
-  
-}
 
   const btnStart = document.querySelector(".btn-start");
   const btnReset = document.querySelector(".btn-reset");
   const btnStop = document.querySelector(".btn-stop");
 
+  function startTimer() {
+      const interval = setInterval(timer, 1000);
+      return interval;
+  }
+
   btnStart.addEventListener("click", startTimer);
   btnReset.addEventListener("click", reset);
   btnStop.addEventListener("click", stopTimer);
 
-  function startTimer() {
-    console.log("-");
-    currentValue--;
-    display.textContent = currentValue;
+  function timer() {
+    if (currentSecond < 59) {
+      seconds.textContent = ++currentSecond;
+    } else if ((currentSecond = 60 & (currentMinute < 59))) {
+      minutes.textContent = ++currentMinute;
+      currentSecond = 0;
+      seconds.textContent = currentSecond;
+    } else if ((currentMinute = 60)) {
+      hours.textContent = ++currentHour;
+      currentMinute = 0;
+      minutes.textContent = currentMinute;
+    }
   }
 
   function reset() {
-    console.log("0");
-    currentValue = 0;
-    display.textContent = currentValue;
-    }
-    
+      console.log("0");
+      currentSecond = 0;
+      seconds.textContent = '00';
+      currentMinute = 0;
+      minutes.textContent = "00";
+      currentHour = 0;
+      hours.textContent = "00";
+  }
+
   function stopTimer() {
-    console.log("+");
-    currentValue++;
-    display.textContent = currentValue;
+    console.log("stop");
+    clearInterval(timer);
   }
 })();
